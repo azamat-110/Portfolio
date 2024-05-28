320198c581510a7e16017a875dda4ad7c7533b70
azamat-110 <tursunbayevazamat565@gmail.com>
2024-05-28 13:59:26 +0500

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
-  plugins: [vue()],
+  plugins: [vue(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: 'src/assets/images',
+        dest: 'assets/images',
+      },
+    ],
+  }),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
