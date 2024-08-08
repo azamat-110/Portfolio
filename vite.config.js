a46d877bb1be6d648a7ef7345260cb18ed0b2f89
azamat-110 <tursunbayevazamat565@gmail.com>
2024-08-08 18:18:12 +0500

    vite config fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
+  base: '/Portfolio/',
   plugins: [vue()],
 });
